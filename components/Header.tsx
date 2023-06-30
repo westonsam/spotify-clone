0c339b663af7faa760dcac81eb7e17fb5d6cebb0
westonsam <132954237+westonsam@users.noreply.github.com>
2023-06-30 15:04:46 -0700

    Fixed Header Routing for Mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -95,6 +95,7 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className="flex md:hidden gap-x-2 gap-y-2 items-center">
                     <button
+                        onClick={() => router.push('/')}
                         className="
                     rounded-full
                     p-2
@@ -107,6 +108,7 @@ const Header: React.FC<HeaderProps> = ({
                         <HiHome className="text-black" size={20} />
                     </button>
                     <button
+                        onClick={() => router.push('/search')}
                         className="
                     rounded-full
                     p-2
